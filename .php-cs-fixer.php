f1be145af67e64973d87b8ff66b8671bea6fc191
hoochicken <mareike.riegel@ql.de>
2024-08-31 15:55:09 +0200

    feat (code quality) php-cs-fixer added

---
+++ .php-cs-fixer.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude([
+        'vendor',
+        'language',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PhpCsFixer' => true,
+    ])
+    ->setFinder($finder)
+;
